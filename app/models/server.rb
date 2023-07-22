b82e485f7412f94f36ad12dca0fc4484e453eaa5
myosotis-dev <github@myosotis.dev>
2023-07-22 23:09:38 +0900

    add server model and test. - music_volume は 0-100 のlengthに決定

--- app/models/server.rb
+++ app/models/server.rb
@@ -16,42 +16,36 @@ class Server < ApplicationRecord
 	# setter
 	def discord_server_id=(param)
 		# MySQL can't save over 20 digits num, so save as string.
+		unless param.kind_of?(Integer)
+			raise TypeError "must be Integer."
+		end
 		i = param.to_s
 		self[:discord_server_id] = i
 	end
 
-	# getter
-	def music_volume
-		self[:music_volume].to_f * 0.01
-	end
-
-	# setter
-	def music_volume=(param)
-		i = (param.to_f * 100).to_i
-		self[:music_volume] = i
-	end
 
 	# --- validate ---
 
 	def valid_discord_server_id?
-		unless self[:discord_server_id].kind_of?(String)
+		str = self[:discord_server_id].freeze
+		unless str.kind_of?(String)
 			errors.add(:discord_server_id, "must be string.")
 			return
 		end
-		str = self[:discord_server_id].freeze
 		unless str == (str.to_i.to_s)
 			errors.add(:discord_server_id, "must be convertable to integer.")
 		end
 	end
 
-	def valid_music_volume?
-		num = self[:music_volume]
+	def valid_music_volume?		
+		num = self[:music_volume].freeze
 		unless num.kind_of?(Integer)
 			errors.add(:music_volume, "must be Integer.")
 			return
 		end
 		unless 0 <= num && num <= 100
 			errors.add(:music_volume, "must be suitable length, 0 <= num && num <= 100 .")
+			return
 		end
 	end
 

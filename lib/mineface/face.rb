a55239671e33c1b4bfabcba3d7481c43ece6c237
myosotis-dev <github@myosotis.dev>
2023-08-19 14:32:41 +0900

    add get_minecraft_profile(), get_skin_image_url()

--- lib/mineface/face.rb
+++ lib/mineface/face.rb
@@ -2,6 +2,7 @@ module Mineface
 	require 'net/http'
 	require 'uri'
 	require "json"
+	require 'base64'
 
 	class Face
 		def initialize options={}
@@ -14,8 +15,44 @@ module Mineface
 
 			_json = request_json("https://api.mojang.com/users/profiles/minecraft/#{name}")
 			raise GetUUIDError, "this user name is not exist." unless _json["errorMessage"].nil?
+			# TODO: include API errorMessage to Exception message.
 
 			return _json["id"]
+		end
+
+		def get_minecraft_profile uuid=""
+			raise ArgumentError unless uuid.kind_of? String
+			raise ArgumentError if uuid == ""
+			
+			_json = request_json("https://sessionserver.mojang.com/session/minecraft/profile/#{uuid}")
+			raise GetProfileError, "this uuid is invalid. #{_json["errorMessage"]}" unless _json["errorMessage"].nil?
+
+			_json_str = Base64.decode64(_json["properties"].first["value"])
+			begin
+				_json["properties"].first["value"] = JSON.parse(_json_str)
+				# TODO: check case of including element"s" on _json["properties"] .
+			rescue JSON::ParserError => e
+				raise GetProfileError, e.message
+			rescue => e
+				warn e.message
+				raise GetProfileError, e.message
+			end
+			return _json
+		end
+
+		def get_skin_image_url uuid=""
+			raise ArgumentError unless uuid.kind_of? String
+			raise ArgumentError if uuid == ""
+
+			_json = get_minecraft_profile uuid
+			_url = _json.try(:[], "properties")
+				.try(:first)
+				.try(:[], "value")
+				.try(:[], "textures")
+				.try(:[], "SKIN")
+				.try(:[], "url")
+			raise GetSkinUrlError, "there is no skin url." if _url.nil?
+			return _url
 		end
 
 		def request_json _url=""
@@ -48,6 +85,18 @@ module Mineface
 
 	class GetUUIDError < StandardError
 		def initialize msg="Can't get UUID."
+			super msg
+		end
+	end
+
+	class GetProfileError < StandardError
+		def initialize msg="Can't get Profile."
+			super msg
+		end
+	end
+
+	class GetSkinUrlError < StandardError
+		def initialize msg="Can't get Profile."
 			super msg
 		end
 	end

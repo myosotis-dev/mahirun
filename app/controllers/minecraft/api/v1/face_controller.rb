bfdcb2977b54d014c7ef8c712de8c05986207cd1
myosotis-dev <github@myosotis.dev>
2023-08-20 01:09:15 +0900

    update face api, return steve face when some error occurred.

--- app/controllers/minecraft/api/v1/face_controller.rb
+++ app/controllers/minecraft/api/v1/face_controller.rb
@@ -1,11 +1,22 @@
 class Minecraft::Api::V1::FaceController < ApplicationController
 	def show
-		@face = face = Mineface::Face.new name: params[:id]
-		@face.request!
-		image_bin = @face.image.to_blob do
-			self.format = "PNG"
+		@face = Mineface::Face.new name: params[:id]
+		begin
+			@face.request!
+			@image_bin = @face.image.to_blob do
+				self.format = "PNG"
+			end
+		rescue => e
+			warn "[WARNING]: #{e.message}"
+			@image_bin = steve_face_image.to_blob do
+				self.format = "PNG"
+			end
 		end
-		response.content_type = "image/png"
-		send_data image_bin
+		send_data @image_bin, type: "image/png", disposition: 'inline'
+	end
+
+	private def steve_face_image
+		@face = Mineface::Face.new
+		return @face.get_face_image "/opt/app/public/steve.png"
 	end
 end
